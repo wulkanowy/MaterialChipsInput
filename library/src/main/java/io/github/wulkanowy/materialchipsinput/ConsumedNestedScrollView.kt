7cccdc678d9e45c64b6c2c02c770c87bb6f22114
Rafał Borcz <RafalBO99@outlook.com>
2019-08-02 20:11:08 +0200

    Add ConsumedNestedScrollView

---
+++ library/src/main/java/io/github/wulkanowy/materialchipsinput/ConsumedNestedScrollView.kt
@@ -0,0 +1,31 @@
+package io.github.wulkanowy.materialchipsinput
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.widget.NestedScrollView
+
+class ConsumedNestedScrollView : NestedScrollView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+
+    constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr)
+
+    override fun onNestedFling(target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
+        return super.onNestedFling(target, velocityX, velocityY, true)
+    }
+
+    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
+        super.onNestedScroll(target, dxConsumed, dyConsumed, 0, 0, type)
+    }
+
+    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
+        super.onNestedScroll(target, dxConsumed, dyConsumed, 0, 0)
+    }
+
+    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int, consumed: IntArray) {
+        super.onNestedScroll(target, dxConsumed, dyConsumed, 0, 0, type, consumed)
+    }
+}
