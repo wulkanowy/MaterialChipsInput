c7482eb50755f91c35932d72064c4abe23db3a23
Dominik Korsa <dominik.korsa@gmail.com>
2020-06-11 14:31:53 +0200

    Fuzzy search (#33)

--- library/src/main/java/io/github/wulkanowy/materialchipsinput/DropdownListViewAdapter.kt
+++ library/src/main/java/io/github/wulkanowy/materialchipsinput/DropdownListViewAdapter.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import io.github.wulkanowy.materialchipsinput.util.createLetterBitmap
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_dropdown_list.*
+import me.xdrop.fuzzywuzzy.FuzzySearch
+import java.util.Locale
 
 internal class DropdownListViewAdapter(private val context: Context) :
     RecyclerView.Adapter<DropdownListViewAdapter.ItemViewHolder>() {
@@ -27,7 +29,8 @@ internal class DropdownListViewAdapter(private val context: Context) :
     override fun getItemCount() = filteredItemList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder(LayoutInflater.from(context).inflate(R.layout.item_dropdown_list, parent, false))
+        return ItemViewHolder(LayoutInflater.from(context)
+            .inflate(R.layout.item_dropdown_list, parent, false))
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
@@ -70,7 +73,8 @@ internal class DropdownListViewAdapter(private val context: Context) :
         override val containerView get() = itemView
     }
 
-    class ItemFilter(private val adapter: DropdownListViewAdapter) : Filter() {
+    class ItemFilter(private val adapter: DropdownListViewAdapter) :
+        Filter() {
 
         @SuppressLint("DefaultLocale")
         override fun performFiltering(constraint: CharSequence): FilterResults {
@@ -80,12 +84,17 @@ internal class DropdownListViewAdapter(private val context: Context) :
             if (constraint.isBlank()) {
                 filteredList.addAll(originalList)
             } else {
-                val pattern = constraint.toString().toLowerCase().trim()
-                originalList.forEach {
-                    if (it.title.toLowerCase().contains(pattern)) {
-                        filteredList.add(it)
+                filteredList.addAll(originalList
+                    .map {
+                        val ratio = FuzzySearch.tokenSortPartialRatio(
+                            constraint.toString().toLowerCase(Locale.getDefault()).trim(),
+                            it.title.toLowerCase(Locale.getDefault())
+                        )
+                        Pair(it, ratio)
                     }
-                }
+                    .sortedByDescending { (_, ratio) -> ratio }
+                    .filter { (_, ratio) -> ratio > 50 }
+                    .map { (it, _) -> it })
             }
 
             return FilterResults().apply {
